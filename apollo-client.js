09a1d3fcd523a344256897fe6531e7b62cce9dc4
R. Potter <rjspotter@gmail.com>
2017-11-21 20:14:49 +0300

    upgrade to apollo v2. commented afterware kept for posterity

--- apollo-client.js
+++ apollo-client.js
@@ -1,4 +1,7 @@
-import ApolloClient, { getFragmentDefinitions, createFragmentMap, createBatchingNetworkInterface } from 'apollo-client';
+import ApolloClient, { getFragmentDefinitions, createFragmentMap, createBatchingNetworkInterface } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloLink } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
 import gql from 'graphql-tag';
 
 /**
@@ -10,24 +13,37 @@ const init = (config) => {
     console.error('Trying to initialize an ApolloClient without having a config set');
   }
 
-  const networkInterface = createBatchingNetworkInterface(Object.assign({batchInterval: 10, batchMax: 10}, config));
+  // const networkInterface = createBatchingNetworkInterface(Object.assign({batchInterval: 10, batchMax: 10}, config));
 
-  const absintheAfterware = {
-    applyBatchAfterware(res, next) {
-      res.responses.forEach((resp) => {
-        resp.data = resp.payload.data;
-      });
+  // const absintheAfterware = {
+  //   applyBatchAfterware(res, next) {
+  //     res.responses.forEach((resp) => {
+  //       resp.data = resp.payload.data;
+  //     });
 
-      next();
-    },
-  };
+  //     next();
+  //   },
+  // };
 
-  networkInterface.useAfter([absintheAfterware]);
+    // networkInterface.useAfter([absintheAfterware]);
+
+  const httpLink = createHttpLink({ uri: '/graphql' });
+
+  // const absintheLink = new ApolloLink((operation, forward) => {
+  //   return forward(operation).map((response) => {
+  //     response.responses.forEach((resp) => {
+  //       resp.data = resp.payload.data;
+  //     });
+  //     return response;
+  //   });
+  // });
+
+  const link = absintheLink.concat(httpLink);
 
   // Create the apollo client
   return new ApolloClient({
-    networkInterface: networkInterface,
-    queryDeduplication: true
+    link: httpLink,
+    cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
   });
 }
 
