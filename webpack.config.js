ee254b55352d7ea361032dbd65b43ccc9cdc2601
Paul Hachmang <paul@h-o.nl>
2017-12-15 13:58:38 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
+const BabiliPlugin = require("babili-webpack-plugin");
 
 module.exports = {
   entry: './apollo-client.js',
   output: {
     library: 'Apollo',
     libraryTarget: 'var',
-    path: __dirname+'/build',
+    path: path.resolve(__dirname+'/build'),
     filename: 'apollo-client.js'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": { NODE_ENV: "'production'" }
+    }),
+    new BabiliPlugin()
+  ],
   module: {
     rules: [
       {
@@ -16,7 +24,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ["env"]
           }
         }
       }
