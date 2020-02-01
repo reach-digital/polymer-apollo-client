5a1cb8f53345a951aa1de2c261257f55ab08e19a
Paul Hachmang <paul@reachdigital.nl>
2020-02-01 14:50:17 +0100

    Modulizer

---
+++ modulizer.bash
@@ -0,0 +1,5 @@
+modulizer --out . \
+--add-import-meta \
+--dependency-mapping apollo-client,apollo-client,2.6.8 \
+--dependency-mapping swiper,swiper,5.3.0 \
+--dependency-mapping matryoshka-loader,@reachdigital/matryoshka-loader,1.0.0 \
